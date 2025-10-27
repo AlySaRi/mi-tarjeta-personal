bd172fdd6480a33ac58430ccd62fdbd34a3146a8
Aly <aly.sari2.5@gmail.com>
2025-10-27 21:07:22 +0100

    Update Mi tarjeta

---
+++ tarjeta.js
@@ -0,0 +1,6 @@
+const myColors = require('colors/safe');
+
+console.log(myColors.yellow('Alicia Sancho')); 
+console.log(myColors.cyan('Valencia, España')); 
+console.log(myColors.green('Quiero aprender programación para poder trabajar de UI designer con Front-End')); 
+console.log(myColors.rainbow('¡Plus Ultra!'));
